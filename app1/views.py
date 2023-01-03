b43471edfc224369048339a546160060d1612f91
Anjali2071207120 <chinthaanjali681@gmail.com>
2023-01-03 19:06:54 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def gajala(request):
+    return render(request,'gajala.html')
+
+
---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def venky(request):
+    return render(request,'venky.html')
+
